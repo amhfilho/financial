426c4d1c82e3870a1f040f0b864350ccd51fc6f0
Antonio Henriques <amhfilho@gmail.com>
2018-10-23 11:35:23 -0300

    Initial commit - Java Spring Boot project

---
+++ myfinance/src/main/java/com/amhfilho/myfinance/MyfinanceApplication.java
@@ -0,0 +1,12 @@
+package com.amhfilho.myfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinanceApplication.class, args);
+	}
+}
