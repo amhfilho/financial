630430052bed3d070a8b916731f700ec6350a1bf
Antonio Mario Henriques Filho <amhfilho@gmail.com>
2018-10-25 11:20:08 -0300

    Operation

--- myfinance/src/main/java/com/amhfilho/myfinance/Operation.java
+++ myfinance/src/main/java/com/amhfilho/myfinance/Operation.java
@@ -1,10 +1,14 @@
 package com.amhfilho.myfinance;
 
+import java.math.BigDecimal;
+
 /**
  * Financial operation, that could result in one or more Transaction
  */
 public class Operation {
 
     private CheckingAccount account;
-
+    private String description;
+    private BigDecimal value;
+    
 }
